e48488fd6234c10d7455273ab64b5c4519a3f0b7
xTrogwaRx <sergh91@mail.ru>
2023-06-09 16:24:11 +0300

    A major update to the reinstallation script
    -Added automatic GUID search for all versions of KOGE;
    -Added a startup Y/N question after the KOGE installation process;
    -Added informers about the current process of the script;
    -A little bit of refactoring.

--- reinstall_koge.cmd
+++ reinstall_koge.cmd
@@ -19,46 +19,32 @@ echo   ▄███████████  ███    █▀     ▀██████▀         888Y    888     888
 echo   ██        ██                                                         
 echo. 
 
-:: При зауске ищет известную версию Koge, удаляет ее и чистит APPDATA\Koge
-:: Если в папке со скриптом есть установщик SetupKoge.exe, устанавливает.
-:: Запускать от администратора
 
-:: Версия 2.2.5
-set version[0]={e2e9db0b-0bac-4498-ac96-dab9e7bbb43d}
-:: Версия 2.2.6
-set version[1]={7f0aa41d-fe4f-417e-9cec-e94e8588a189}
-:: Версия 2.2.7
-set version[2]={58f6cca6-9d74-4aba-857d-2e61d2d06444}
-:: Версия 2.2.8
-set version[3]={eaad089e-b385-4a7c-8143-dd31f3aca23d}
-:: Версия 3.2.1
-set version[4]={164c8200-dcf9-4be1-ad0f-b4e1f1bd88d5}
-:: Версия 2.4.1
-set version[5]={b516ae86-bc4d-41f1-9212-3bdd23c6b1ca}
-:: Версия 2.5.2
-set version[6]={edcfee75-5b6c-4e26-b4c1-8d830b8adcfa}
-:: Версия 2.6.1
-set version[7]={15788722-d009-4062-bd41-0b482ab6ddfa}
-:: Версия 2.7.1
-set version[8]={6d727c04-2734-4234-aeed-62d4c6c420e6}
-:: заглушка
-set version[9]=dummy
+:elevate_privileges
+	echo Повышение привелегий...
+	ver |>NUL find /v "5." && if "%~1"=="" (
+	  Echo CreateObject^("Shell.Application"^).ShellExecute WScript.Arguments^(0^),"1","","runas",1 >"%~dp0elevating.vbs"
+	  cscript.exe //nologo "%~dp0elevating.vbs" "%~f0"& goto :eof
+	)
 
-ver |>NUL find /v "5." && if "%~1"=="" (
-  Echo CreateObject^("Shell.Application"^).ShellExecute WScript.Arguments^(0^),"1","","runas",1 >"%~dp0Elevating.vbs"
-  cscript.exe //nologo "%~dp0Elevating.vbs" "%~f0"& goto :eof
-)
+:get_GUID
+	echo Получение GUID...
+	powershell -Command "Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like 'ОГЭ в компьютерной форме*'}" > temp
+	for /F "tokens=1,3 delims= " %%i in (temp) do set %%i=%%j
 
-openfiles > NUL 2>&1
-if NOT %ERRORLEVEL% EQU 0 (
-	goto not_admin
-) else (
-	goto go_next
-)
+:check_privileges
+	echo Проверка привелегий...
+	openfiles > NUL 2>&1
+	if NOT %ERRORLEVEL% EQU 0 (
+		goto not_admin
+	) else (
+		goto go_next
+	)
 
 :not_admin
 	Color 60
 	echo Этот скрипт должен быть запущен от имени администратора!
+	pause
 	goto end
 
 :go_next
@@ -70,17 +56,21 @@ if NOT %ERRORLEVEL% EQU 0 (
 	)
 
 	set /A i=0
-:enum_versions 
-    if defined version[%i%]  (
-		set key=%uninstall_key%%%version[%i%]%%
+:enum_versions
+    if defined IdentifyingNumber  (
+		for /F "Tokens=7* delims=\" %%I In ('Reg Query HKLM\SOFTWARE\Classes\Installer\Dependencies\%IdentifyingNumber%\Dependents /s') Do Set "UnistalNumber=%%J"
+	
+		set key=%uninstall_key%%UnistalNumber%
+
 		for /F "Tokens=2*" %%I In ('Reg Query "HKLM\%key%" /V "DisplayVersion" 2^>nul') Do Set "InstalledVersion=%%J"
 		for /F "Tokens=2*" %%I In ('Reg Query "HKLM\%key%" /V "BundleCachePath" 2^>nul') Do Set "UninstallString=%%J"
-		for /F "Tokens=2*" %%I In ('Reg Query "HKLM\%key%" /V "DisplayName" 2^>nul') Do Set "DisplayName=%%J"		
-		if defined UninstallString goto :uninstall_job
+		for /F "Tokens=2*" %%I In ('Reg Query "HKLM\%key%" /V "DisplayName" 2^>nul') Do Set "DisplayName=%%J"
+		
+		if defined UninstallString goto uninstall_job
 		set /A i=%i%+1
-		goto :enum_versions
+		goto enum_versions
     ) else (
-		goto :clear_data
+		goto clear_data
 	)	
 	
 :uninstall_job
@@ -94,18 +84,45 @@ if NOT %ERRORLEVEL% EQU 0 (
 	) else (
 		echo Папка APPDATA\Koge не найдена
 	)
-	
+
 :install_new
 	if exist "%~dp0SetupKoge.exe" (
 		echo Найден установщик SetupKoge.exe. Устанавливаю...
 		start /wait "" "%~dp0SetupKoge.exe" /S
 		Color 27
-		del "%~dp0Elevating.vbs"
+		
+		goto start_promt
 	) else (
-		Color 60
-		del "%~dp0Elevating.vbs"
+		echo Установщик SetupKoge.exe не найден. При необходимости запустите установщик вручную.
+		pause
+		goto end
 	)
+
+
+:start_promt
+	setlocal
+	set res=null
+	:promt
+	SET /P AREYOUSURE=Запустить "ОГЭ в компьютерной форме" после завершения скрипта? (Y/N)?
+
+	if /I "%AREYOUSURE%" == "N" set res=false
+	if /I "%AREYOUSURE%" == "Н" set res=false
+	if /I "%AREYOUSURE%" == "Y" set res=true
+	if /I "%AREYOUSURE%" == "Д" set res=true
+
+	if "%res%"=="false" ( 
+		GOTO end_promt
+	)
+	if not "%res%"=="true"  (
+		GOTO promt
+	)
+
+	start explorer.exe "C:\Users\Public\Desktop\ОГЭ в компьютерной форме.lnk"
+
+	:end_promt
+	endlocal
+	
 	
 :end
-pause
-	
+del "%~dp0elevating.vbs"
+del "temp"
